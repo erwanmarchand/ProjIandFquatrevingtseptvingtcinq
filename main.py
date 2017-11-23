b4e54730f75ff86625ecc2e4a65901a2b3f9aeee
Thibault Piana <piana.thibault@gmail.com>
2017-11-22 23:34:22 -0500

    Changement du dossier image pour plus de clarté

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
-from lib.ImageManager import *
 from lib.ImageProcessor import *
 from lib.analysis.PyramidAnalyzer import *
 from lib.debug.Log import *
-import cv2
+
+import lib.debug.Log as Log_file
+import numpy as np
 
 IMAGES_PATH = "images/"
-ORIGINAL_IMAGES_PATH = IMAGES_PATH + "original/"
-GENERATED_IMAGES_PATH = IMAGES_PATH + "generated/"
-ANALYSIS = True
-DEBUG = True
 NAME_PICTURE = 'lena2.jpg'
 
+ANALYSIS = True
+DEBUG = True
+
 s = 3
-octave = 5
-
-
+octave = 3
 
 #  On charge l'image et on la redimensionne
+Log_file.DEBUG_ACTIVATED = DEBUG
 Log.debug("Démarrage...")
 Log.debug("Chargement de l'image")
-imgWithColor = ImageManager.loadMatrix(ORIGINAL_IMAGES_PATH + NAME_PICTURE)
-Log.debug("Redimensionnement de l'image")
+imgWithColor = ImageManager.loadMatrix(IMAGES_PATH + NAME_PICTURE)
+Log.debug("Image chargée. Redimensionnement de l'image")
 imgWithColor = ImageManager.getOctave(imgWithColor, -1)
 
 # On convertit l'image en nuances de gris pour travailler dessus
@@ -36,6 +35,13 @@ if ANALYSIS:
     analyzer.greyscalePicture = imgGreyscale
 else:
     analyzer = None
+
+# On vérifie que le nombre d'octave n'st pas trop grand
+octave_debug = min(int(np.log2(imgGreyscale.shape[0])), int(np.log2(imgGreyscale.shape[1])), octave)
+if octave_debug != octave:
+    Log.info("Le nombre d'octave a été changé à " + str(
+        int(octave_debug)) + " afin d'éviter les problèmes de redimensionnement")
+    octave = octave_debug
 
 # On applique l'algorithme
 keypoints = ImageProcessor.findKeypoints(imgGreyscale, s, octave,
