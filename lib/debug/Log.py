a633c97bbcc43455d9cf2ae6cbf6c0c6a22f4d3a
Thibault Piana <piana.thibault@gmail.com>
2017-11-22 23:34:55 -0500

    Mise a jour du fichier de log pour une utilisation plus simple et un support de windows

--- lib/debug/Log.py
+++ lib/debug/Log.py
@@ -13,22 +13,21 @@ ERROR = 4
 FATAL = 5
 
 CSI = "\x1B["
+DEBUG_ACTIVATED = True
 
 import sys
 import time
+import platform
 
 
 class Log:
     @staticmethod
-    def _log(level, msg, args):
+    def _log(level, msg, indentation):
+        msg = ("    " * indentation) + msg
+
         if level not in (DEBUG, INFO, WARN, ERROR, FATAL):
             raise ValueError('%s wrong log level' % str(level))
 
-        COLOR = "31m" if level in [WARN, ERROR, FATAL] else "0m"
-        COLOR = "34m" if level in [INFO] else "0m"
-        COLOR = "32m" if level in [DEBUG] else "0m"
-
-        msg = msg % args if args else msg
         stream = sys.stderr if level in (WARN, ERROR, FATAL) else sys.stdout
 
         if stream.errors == 'strict':
@@ -36,29 +35,43 @@ class Log:
             encoding = stream.encoding
             msg = msg.encode(encoding, "backslashreplace").decode(encoding)
 
-        stream.write(CSI + COLOR + '[' + str(round(time.clock(), 2)) + ']' + CSI + "0m" + " %s\n" % msg)
+        if platform.system() == "Windows":
+            stream.write('[' + str(round(time.clock(), 2)) + ']' + " %s\n" % msg)
+        else:
+            COLOR = "31m" if level in [WARN, ERROR, FATAL] else "0m"
+            COLOR = "34m" if level in [INFO] else "0m"
+            COLOR = "32m" if level in [DEBUG] else "0m"
+
+            stream.write(CSI + COLOR + '[' + str(round(time.clock(), 2)) + ']' + CSI + "0m" + " %s\n" % msg)
+
         stream.flush()
 
     @staticmethod
     def log(level, msg, *args):
-        Log._log(level, msg, args)
+        indentation = args[0] if len(args) > 0 else 0
+        Log._log(level, msg, indentation)
 
     @staticmethod
     def debug(msg, *args):
-        Log._log(DEBUG, msg, args)
+        indentation = args[0] if len(args) > 0 else 0
+        if DEBUG_ACTIVATED:
+            Log._log(DEBUG, msg, indentation)
 
     @staticmethod
     def info(msg, *args):
-        Log._log(INFO, msg, args)
+        indentation = args[0] if len(args) > 0 else 0
+        Log._log(INFO, msg, indentation)
 
     @staticmethod
     def warn(msg, *args):
-        Log._log(WARN, msg, args)
+        Log._log(WARN, msg, indentation)
 
     @staticmethod
     def error(msg, *args):
-        Log._log(ERROR, msg, args)
+        indentation = args[0] if len(args) > 0 else 0
+        Log._log(ERROR, msg, indentation)
 
     @staticmethod
     def fatal(msg, *args):
-        Log._log(FATAL, msg, args)
+        indentation = args[0] if len(args) > 0 else 0
+        Log._log(FATAL, msg, indentation)
