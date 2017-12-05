be740d9eb01415780f891ed81afc8e4312d2dcc2
Thibault Piana <piana.thibault@gmail.com>
2017-12-05 15:04:01 -0500

    Mise a jour du fichier de test général

--- generateAnalysis.py
+++ generateAnalysis.py
@@ -1,78 +1,43 @@
 # -*- coding: utf-8 -*-
+"""
+    Ce fichier permet de générer l'analyse des points clés de plusieurs fichiers, avec plusieurs paramètres
+"""
+
 from lib.ImageProcessor import *
 from lib.analysis.PyramidAnalyzer import *
 from lib.debug.Log import *
 
 import lib.debug.Log as Log_file
-import numpy as np
+import lib.Utils as Utils_file
 
+# Configuration générale
 IMAGES_PATH = "images/"
-ANALYSIS = True
-DEBUG = True
-S = 3
-Log_file.DEBUG_ACTIVATED = DEBUG
+OUT_DIR = "out/"
+Log_file.DEBUG_ACTIVATED = True  # False si vous voulez que les logs soit désactivés
+Utils_file.ACTIVATE_LOAD_BAR = True  # False si vous voulez que les load bar soit désactivés (sous windows notamnent)
 
-NAME_PICTURE = 'gauche.jpg'
-# Image de gauche, 4 octaves
+# Paramètres de tests
+PICTURES = ["gauche.jpg", "droite.jpg", "notre-dame.png", "lena.jpg"]
+CONSTRAST_ARGS = [0.01, 0.02, 0.03]
+HESSIAN_ARGS = [5, 7, 10]
+NB_OCTAVES = [1, 4]
 
-#  On charge l'image
-Log.debug("Démarrage...")
-Log.debug("Chargement de l'image")
-image = ImageManager.loadMatrix(IMAGES_PATH + NAME_PICTURE)
+#  Execution
+for p in PICTURES:
+    for n in NB_OCTAVES:
+        for h in HESSIAN_ARGS:
+            for c in CONSTRAST_ARGS:
+                print("#########################")
+                p_name = p.split('.')[0]
 
-# On applique l'algorithme
-analyzer = PyramidAnalyzer("out_gauche_4/")
-ImageProcessor.findKeypoints(image, S, 4,
-                             pyramid_analyzer=analyzer,
-                             minimum_contrast=0.03,
-                             r_courb_principal=10)
+                Log.debug("Chargement de l'image " + p_name)
+                image = ImageManager.loadMatrix(IMAGES_PATH + p)
 
-analyzer.analyze()
+                # On applique l'algorithme
+                analyzer = PyramidAnalyzer(OUT_DIR + p_name + "_" + str(n) + "_" + str(c) + "_" + str(h) + "/")
+                ImageProcessor.findKeypoints(image, 3, n,
+                                             pyramid_analyzer=analyzer,
+                                             minimum_contrast=c,
+                                             r_courb_principal=h)
 
-# Image de gauche, 1 octaves
-#  On charge l'image
-Log.debug("Démarrage...")
-Log.debug("Chargement de l'image")
-image = ImageManager.loadMatrix(IMAGES_PATH + NAME_PICTURE)
-
-# On applique l'algorithme
-analyzer = PyramidAnalyzer("out_gauche_1/")
-ImageProcessor.findKeypoints(image, S, 1,
-                             pyramid_analyzer=analyzer,
-                             minimum_contrast=0.03,
-                             r_courb_principal=10)
-
-analyzer.analyze()
-
-# --------------------------------------------------- #
-
-NAME_PICTURE = 'droite.jpg'
-# Image de droite, 4 octaves
-#  On charge l'image
-Log.debug("Démarrage...")
-Log.debug("Chargement de l'image")
-image = ImageManager.loadMatrix(IMAGES_PATH + NAME_PICTURE)
-
-# On applique l'algorithme
-analyzer = PyramidAnalyzer("out_droite_4/")
-ImageProcessor.findKeypoints(image, S, 4,
-                             pyramid_analyzer=analyzer,
-                             minimum_contrast=0.03,
-                             r_courb_principal=10)
-
-analyzer.analyze()
-
-# Image de droite, 1 octaves
-#  On charge l'image
-Log.debug("Démarrage...")
-Log.debug("Chargement de l'image")
-image = ImageManager.loadMatrix(IMAGES_PATH + NAME_PICTURE)
-
-# On applique l'algorithme
-analyzer = PyramidAnalyzer("out/out_droite_1/")
-ImageProcessor.findKeypoints(image, S, 1,
-                             pyramid_analyzer=analyzer,
-                             minimum_contrast=0.03,
-                             r_courb_principal=10)
-
-analyzer.analyze()
+                analyzer.analyze()
